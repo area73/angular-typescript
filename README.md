e5d3bd87ab0cd221fa1479393664ddf513bbdceb
rerades <rerades@siete3.com>
2015-09-27 18:06:43 +0200

    final

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # angular-typescript
-Charla demostración Typescript 
+
+## Descripción de la rama
+1) Vamos a crearnos nuestro primer archivo TypeScript
+2) utilización del compilador --> tsc
+ 
+NOTA: para clonar ramas --> git clone https://github.com/area73/angular-typescript.git --branch pr03c ./05
 
 
---
+++ www/js/test.ts
@@ -0,0 +1,43 @@
+/**
+ * Created by rerades on 25/9/15.
+ */
+
+
+/**
+ * @description
+ * Vamos a crearnos nuestro primer Hola Universo
+ * utilizaremos el compilador
+ * tsc test.js
+ * Veremos los parámetros del compilador tsc -h
+ *
+ */
+
+// TODO
+// document.write("<h1>Hola Universo</h1>");
+
+/**
+ *
+ * @description
+ * Vamos a ver otros parámetros importantes del compilador
+ *    --sourceMap
+ *    -w --watch
+ *    -m --module   amd o commonjs
+ *    -t
+ */
+
+// TODO  [comentar arriba]
+//import m = require('mod');
+//export  var t = m.something + 1;
+//
+//module xyz {
+//    document.write("<h1>Hola Universo</h1>");
+//}
+
+/**
+ * @description
+ *
+ *  en TS 1.5 existe un json de configuración  (renombrar tsconfig.json)
+ *
+ */
+
+
